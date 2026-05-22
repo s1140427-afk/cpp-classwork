86aa2acb49f467a00b6cc8dbf17ccf2d64f7150a
s1140427-afk <s1140427@dmhs.kh.edu.tw>
2026-05-22 10:55:39 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "1.幸福威孟寶" << endl;
+	cout << "2.幸福 威孟寶" << endl;
+	cout << "3.幸福\t威孟寶" << endl;
+	cout << "4.幸福\n威孟寶" << endl;
+	cout << "\"5.幸福威孟寶\"" << endl;
+	cout << "\'6.幸福威孟寶\'" << endl;
+	cout << "\\7.幸福威孟寶\\" << endl;
+	return 0; 
+}
